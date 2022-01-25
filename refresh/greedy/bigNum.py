fb039a49aa2538880148076b0bfa56db153ca5a7
minhye <anniesmh@gmail.com>
2022-01-26 01:02:39 +0900

    greedy again

---
+++ refresh/greedy/bigNum.py
@@ -0,0 +1,19 @@
+n, m, k = map(int, input().split())
+lst = sorted(list(map(int, input().split())),reverse=True)
+sum = 0 # 총 합
+idx = 0
+prev = 0 # 연속 횟수
+
+# print(lst)
+while m != 0: # 총 더할 횟수
+    # print('m',m, 's',sum)
+    if k == prev: # 연속 횟수가 최대 연속 가능 수와 같으면
+        sum += lst[int(not(idx))]  # 두번째 큰수를 한번 더한다
+        # print('+',lst[int(not(idx))])
+        prev = 0 # 연속 횟수 초기화
+    else:
+        sum += lst[idx] # 아닐경우, 연속 가능 횟수만큼 최대값 더하기
+        # print(lst[idx])
+        prev += 1 # 연속 횟수 증가
+    m -= 1 # 더하기 연산 수 감소
+print(sum)
