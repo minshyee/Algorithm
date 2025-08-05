b7db713669c280886edb880bf3da96df0490f50c
Minhye Song <50479962+minshyee@users.noreply.github.com>
2025-08-05 15:25:08 +0900

    Title: 문자에 따른 명령 2, Time: 51ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250805/문자에 따른 명령 2/text-based-commands2.py
+++ 250805/문자에 따른 명령 2/text-based-commands2.py
@@ -3,19 +3,19 @@ dirs = input()
 # Please write your code here.
 dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
 
-dir = 0
-nx, ny = 0, 0
+
+d, nx, ny = 0, 0, 0
 
 coord = 's'
 
 for c in dirs:
     if(c == 'L'):
-        dir = (dir + 1) % 4
+        d = (d + 1) % 4
     elif(c == 'R'):
-        dir = (dir -1 + 4) % 4
+        d = (d -1 + 4) % 4
     elif(c == 'F'):
-        nx += dx[dir]
-        ny += dy[dir]
+        nx += dx[d]
+        ny += dy[d]
 
 print(nx, ny)
         
