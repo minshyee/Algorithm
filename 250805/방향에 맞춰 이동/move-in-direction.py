c1067e58cc57d9a3368890cd34e3545177a540bb
Minhye Song <50479962+minshyee@users.noreply.github.com>
2025-08-05 15:46:46 +0900

    Title: 방향에 맞춰 이동, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250805/방향에 맞춰 이동/move-in-direction.py
+++ 250805/방향에 맞춰 이동/move-in-direction.py
@@ -5,10 +5,6 @@ dist = [int(move[1]) for move in moves]
 
 # Please write your code here.
 x, y = 0, 0
-
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
-
 dx = {'E': 1, 'S': 0, 'W': -1, 'N': 0}
 dy = {'E': 0, 'S': -1, 'W': 0, 'N': 1}
 
