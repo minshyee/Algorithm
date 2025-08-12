f82c99159cc840caaab576a7fc05d9f207a99295
Minhye Song <50479962+minshyee@users.noreply.github.com>
2025-08-12 15:10:11 +0900

    Title: 오름 내림차순 정렬, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250812/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,16 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+# Please write your code here.
+def sortArr(n):
+    return sorted(n)
+
+def reverseArr(n):
+    n.sort()
+    return list(reversed(n))
+
+def printStr(arr):
+    return " ".join(map(str, arr))
+
+print(printStr(sortArr(nums)))
+print(printStr(reverseArr(nums)))
