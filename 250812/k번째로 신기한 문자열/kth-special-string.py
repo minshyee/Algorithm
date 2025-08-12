146b5da73b16f0931228667d3df1fc411b3bfdc4
Minhye Song <50479962+minshyee@users.noreply.github.com>
2025-08-12 17:33:34 +0900

    Title: k번째로 신기한 문자열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250812/k번째로 신기한 문자열/kth-special-string.py
+++ 250812/k번째로 신기한 문자열/kth-special-string.py
@@ -3,6 +3,6 @@ n, k = int(n), int(k)
 str = [input() for _ in range(n)]
 
 # Please write your code here.
-arr = [i for i in str if t in i]
+arr = [i for i in str if i.startswith(t)]
 arr.sort()
 print(arr[k-1])
